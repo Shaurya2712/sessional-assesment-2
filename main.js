fd379899e7b890d6fc07abd935a22696c2f9935b
shaurya2712 <iamshauryamalhotra@gmail.com>
2022-08-11 17:57:40 +0530

    fetched the api to the UI

--- main.js
+++ main.js
@@ -39,3 +39,34 @@ var typedAbout = new Typed(".auto-type-about", {
   loop: true,
   showCursor: false,
 });
+
+var btn = document.getElementById("btn").addEventListener("click", getPost);
+var con = 0;
+var div = document.getElementById("cardDiv");
+
+function getPost() {
+  fetch(
+    "https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09"
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((post) => {
+      for (let index = 0; index < 10; index++) {
+        div.innerHTML += `
+            <div class=" col-12 my-3 mx-auto bg-transparent">
+                <div class="card-body border border-3 border-dark">
+                <h3 class="card-title fw-normal">UserId: ${post[con].userId} </h3>
+                <h3 class="card-title fw-normal ">Id: ${post[con].id} </h3>
+                <h3 class="card-title">Title: ${post[con].title}</h3>
+                <p class="card-text fst-italic fs-3">${post[con].body}</p>
+            </div>
+
+            `;
+        con = con + 1;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
